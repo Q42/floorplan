a166aa4fd344d9487a62e079a6c2bdab131c3cd1
Bob van Oorschot <bob@q42.nl>
2016-09-23 11:59:26 +0200

    changed the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-DEPLOY_HOSTNAME=galaxy.meteor.com meteor deploy timetastic.q42.com --settings settings.json
+DEPLOY_HOSTNAME=galaxy.meteor.com meteor deploy floorplan.q42.com --settings settings.json
